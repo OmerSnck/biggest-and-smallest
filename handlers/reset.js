16b326a3c4fbbb755cbdbd5c1493cb1b58ad0d47
Omer <hbaharkus@gmail.com>
2020-02-28 16:33:43 +0100

    completed

--- handlers/reset.js
+++ handlers/reset.js
@@ -1,9 +1,12 @@
 function resetHandler() {
   debugger;
   // reset state to initial values
-
+  numbers.current = 0;
+	numbers.past = [];
   // re-render UI with values saved in state
-  
+  document.getElementById("biggest").innerHTML = 0;
+  document.getElementById("smallest").innerHTML = 0;
+  document.getElementById("input").value = numbers.current;
   // log interaction: handler name, new state
   log.push({
     handler: 'reset',
--- handlers/save-number.js
+++ handlers/save-number.js
@@ -1,14 +1,21 @@
 function saveNumberHandler() {
   debugger;
   // read new number from user input
-  
+  let lastnumber= Number(document.getElementById('input').value);
+  numbers.all.push(numbers.current);
   // read from state the data you will need for the next step
   // find the new biggest and smallest values
-  
+  if ( lastnumber > numbers.biggest) {
+    numbers.biggest=lastnumber;
+  }
+  if (lastnumber < numbers.smallest) {
+    numbers.smallest=lastnumber;
+  } 
   // update state: new biggest, new smallest, new current & save the last current
  
   // re-render the user interface with values stored in state
-  
+  document.getElementById("biggest").innerHTML = numbers.biggest;
+  document.getElementById("smallest").innerHTML = numbers.smallest;
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'save number',
